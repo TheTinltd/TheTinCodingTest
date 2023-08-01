f15467168f1d69946ee5cc2a6b72ffb51bbc07f0
Mike Garside <mike.garside@thetin.net>
2023-08-01 13:14:16 +0100

    added ioc

---
+++ TheTinTest/AppStart/IOC.cs
@@ -0,0 +1,12 @@
+﻿namespace TheTinTest.AppStart
+{
+    public static class IOC
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            
+
+            return services;
+        }
+    }
+}
